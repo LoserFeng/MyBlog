8c7f447d26ccb118b40f31600172405bbf2e2613
loserfeng <feng_yufan@163.com>
2022-09-02 17:49:43 +0800

    OK 2022.9.2 先备份一份

---
+++ MyBlog.Thread_TEST/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Timers;
+using System.Threading;
+
+namespace MyBlog.Thread_TEST
+{
+
+    public class program
+    {
+        public static TimeSpan waitTime = new TimeSpan(0, 0, 1);
+
+        public static void Main()
+        {
+            Thread thread = new Thread(Work);
+            thread.Start();
+
+
+            if (thread.Join(waitTime + waitTime))
+            {
+                Console.WriteLine("New thread terminated");
+
+            }
+            else
+            {
+                Console.WriteLine("Join Timed out");
+
+            }
+            Thread.Sleep(1000);
+        }
+
+        private static void Work(object? obj)
+        {
+            Console.WriteLine("12");
+            Thread.Sleep(0);
+            Console.WriteLine("23");
+            Thread.Sleep(waitTime*3);
+
+            Console.WriteLine("are you ok?");
+
+        }
+
+    }
+   
+}
